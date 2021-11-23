78984876230e35cca690a3f88f288f1d005ea532
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2021-11-23 22:37:59 +0700

    Create MemberPlatinum.java

---
+++ RentalConsoleGame/src/rentalconsolegame/MemberPlatinum.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalconsolegame;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MemberPlatinum extends MemberGold{
+    int bonus;
+    
+    MemberPlatinum() {
+        rentCost = 45000;
+        point = 10;
+        disc = 3;
+        cashback = 10000;
+    }
+    
+    protected int getBonus(int rentalDuration) {
+        
+        bonus = rentalDuration * 5000;
+        return bonus;
+    }
+}
